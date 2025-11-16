e9a445510774c863f892ccf2e110a6fc82cb1f18
Dri0308 <adriana.rn030812@gmail.com>
2025-11-15 23:32:52 -0300

    Valida os eventos do cadastro de voluntários

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ document.addEventListener(DOMContentLoaded, () => {
         if(menu) menu.classList.remove('active'); // Fecha o menu após clicar em um link
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        const form = document.getElementById("volunteerForm");
+        if(!form) return;
+
+       if (form.dataset.submmiting == 'true') return; //Evitando sobreposição de usuários
+       form.dataset.submmiting = 'true';
+
+       const nome = form.nome.value.trim();//O método trim() remove espaços em branco do início e do fim da string.
+       const email = form.email.value.trim();
+
+       if( !nome || !email){
+        alert("Por favor, preencha os campos obrigatórios.");
+        form.dataset.submmiting = 'false'; //Se for vazio, não grava os dados.
+        return;
+       }
+
