7222779cd11e321da48787a23637e6f7ca9eb0f9
Dri0308 <adriana.rn030812@gmail.com>
2025-11-15 23:31:43 -0300

    Suaviza o scroll para as seções

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ document.addEventListener(DOMContentLoaded, () => {
     }
     window.toggleMenu = toggleMenu;
 });
+ const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if(!section) return;
+        const headerHeight = 70; // Altura fixa do cabeçalho
+        const sectionPosition = section.offsetTop - headerHeight;
+        window.scrollTo({top: sectionPosition, behavior: 'smooth'});
+
+        const menu = document.getElementById('navMenu');
+        if(menu) menu.classList.remove('active'); // Fecha o menu após clicar em um link
+    }
+
